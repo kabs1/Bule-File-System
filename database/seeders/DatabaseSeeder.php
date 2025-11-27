97e3a4373d62806e932de470dcee2b0c86289fb4
kabs1 <30595858+kabs1@users.noreply.github.com>
2025-11-27 08:57:16 +0300

    Fix: Call RolesAndPermissionsSeeder before assigning roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(RolesAndPermissionsSeeder::class);
+
         // Create a super user
         $superUser = User::firstOrCreate(
             ['email' => 'superadmin@example.com'],
